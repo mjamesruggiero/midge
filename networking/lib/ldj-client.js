c0892fd12b1fa7937393a63daa19148d91adc4f5
mjamesruggiero <mjamesruggiero@gmail.com>
2018-04-09 17:21:13 -0700

    Can communicate via sockets

---
+++ networking/lib/ldj-client.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6 */
+/*jslint node: true */
+
+'use strict';
+const EventEmitter = require('events').EventEmitter;
+class LDJClient extends EventEmitter {
+    constructor(stream) {
+        super();
+        let buffer = '';
+        stream.on('data', data => {
+            buffer += data;
+            let boundary = buffer.indexOf('\n');
+            while(boundary !== -1) {
+                const input = buffer.substring(0, boundary);
+                buffer = buffer.substring(boundary + 1);
+                this.emit('message', JSON.parse(input));
+                boundary = buffer.indexOf('\n');
+            }
+        });
+    }
+
+    static connect(stream) {
+        return new LDJClient(stream);
+    }
+}
+module.exports = LDJClient;
---
+++ networking/net-watcher-ldj-client.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+/*jslint node: true */
+
+'use strict';
+const netClient = require('net').connect({port: 60300});
+const ldjClient = require('./lib/ldj-client.js').connect(netClient);
+
+ldjClient.on('message', message => {
+    if (message.type === 'watching') {
+        console.log(`Now watching ${message.file}`);
+    } else if(message.type === 'changed') {
+        console.log(`File changed: ${new Date(message.timestamp)}`);
+    } else {
+        throw new Error(`Unrecognized message type ${message.type}`);
+    }
+});
